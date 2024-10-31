698586eeee96590116a7ee2bd56d2f5f0b500b83
techwithdunamix <techwithdunamix@gmail.com>
2024-10-31 01:08:15 +0100

    finished backend for now

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,21 @@
-from fastapi import FastAPI,File,UploadFile,responses
+from fastapi import FastAPI,File,UploadFile,responses,Request,Response
+from fastapi.middleware.cors import CORSMiddleware
 from fileservices import convert_word_to_csv
 import uuid
 import os
 app = FastAPI()
+
+
+origins :list = ["http://localhost:3000"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
+
+
 UPLOAD_URL = "download/file"
 @app.post("/file/upload")
 async def upload_file(file :UploadFile = File(...)):
@@ -23,7 +36,7 @@ async def upload_file(file :UploadFile = File(...)):
         convert_word_to_csv(temp_file,output_file=otuput_file_name)
         
         os.remove(temp_file)
-        return responses.JSONResponse({"filename":filename})
+        return responses.JSONResponse({"filename":f'{filename}.csv'},status_code=201)
 
 
     except Exception as e:
@@ -32,9 +45,14 @@ async def upload_file(file :UploadFile = File(...)):
 
 
 
-@app.get("get/file/{filename}")
+@app.get("/get/file/{filename}")
 async def get_file_name(filename :str):
-    file_path :str = os.path.join(UPLOAD_URL,filename)
-    print(file_path)
+    file_path  = os.path.join(UPLOAD_URL,filename)
+    try:
+        with open(file_path,mode="r") as file:
+            return responses.FileResponse(file_path)
+    except FileNotFoundError:
+        
+        return responses.JSONResponse("File does not exists .", status_code=404)
     return {"text" : "hi "}
     
